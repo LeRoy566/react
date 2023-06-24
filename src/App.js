1aedef19dbc7c7a9326da4f429c3039066c94374
CyberCaban <andrewgamer63@gmail.com>
2023-06-24 17:09:26 +0300

    Реакт 1 опыт

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [task, setTask] = useState(["боба", "биба", "бэба"]);
+  const elements = task.map((item, i) => {
+    return (
+      <li
+        onClick={() => {
+          deleteTask(i);
+        }}
+        key={i}
+      >
+        {item}
+      </li>
+    );
+  });
+
+  return <ul>{elements}</ul>;
 }
 
 export default App;
